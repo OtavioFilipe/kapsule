893d606a0222a0ca3ee0d228294bdcb0fa5d58a9
OtavioFilipe <darksnack16@gmail.com>
2022-09-23 00:04:41 -0300

    create template cards

--- src/components/molecules/card/index.tsx
+++ src/components/molecules/card/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Image} from 'react-native';
 
 import * as Component from './styles';
 
 const Card: React.FC = () => {
   return (
     <Component.Container>
-      <Text>render</Text>
+      <Component.CardTop>
+        <Component.ChipIcon colors={['#3135C6', '#00AAFF']}>
+          <Image source={require('../../../assets/iconsCard/star.png')} />
+        </Component.ChipIcon>
+        <Component.ChipIconDefault>
+          <Image
+            source={require('../../../assets/iconsCard/exclamation.png')}
+          />
+        </Component.ChipIconDefault>
+        <Component.Capsule>
+          <Image source={require('../../../assets/iconsCard/capsule.png')} />
+          <Component.Title> 30</Component.Title>
+        </Component.Capsule>
+      </Component.CardTop>
+      <Component.CardContent>
+        <Component.CardTitle>Valdispert Mélatonine 1,9 mg </Component.CardTitle>
+        <Component.SubTitle>Votré compatibilité 90/100</Component.SubTitle>
+      </Component.CardContent>
     </Component.Container>
   );
 };
--- src/components/molecules/card/styles.ts
+++ src/components/molecules/card/styles.ts
@@ -1,17 +1,80 @@
+import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
+
 export const Container = styled.TouchableOpacity`
+    margin-top: 16px;
     background: #FFFFFF;
     box-shadow: 0px 6px 6px #C6C9D6;
     border-radius: 8px;
     padding-horizontal: 17px;
     padding-vertical: 16px;
-    shadowColor: "#000",
-    shadowOffset: {
-        width: 0 height: 2
-    };
-    shadowOpacity: 0.25;
-    shadowRadius: 3.84;
+    
+    shadow-opacity: 0.25;
+    shadow-radius: 3.84;
 
     elevation: 5;
+    shadow-offset: {  width: 10,  height: 10  };
+    shadow-color: black;
+    shadow-opacity: 1.0;
 `;
+
+export const CardTop = styled.View`
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+`;
+
+export const ChipIcon = styled(LinearGradient)`
+    width: 24px;
+    height: 24px;
+    align-items: center;
+    justify-content: center;
+    border-radius: 3px;
+    margin-right: 6px;
+`;
+
+export const ChipIconDefault = styled.View`
+    width: 24px;
+    height: 24px;
+    align-items: center;
+    justify-content: center;
+    border-radius: 3px;
+    flex-direction: row;
+    margin-right: 6px;
+`
+export const Capsule = styled.View`
+    width: 46px;
+    height: 25px;
+    background-color: #F5F7FE;
+    flex-direction: row;
+    align-items: center;
+    padding: 3px;
+`
+
+export const Title = styled.Text`
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 22px;
+`
+export const CardContent = styled.View`
+    margin-top: 14px;
+`
+
+export const CardTitle = styled.Text`
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 22px;
+`
+
+export const SubTitle = styled.Text`
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 22px;
+`
