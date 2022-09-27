572363060ca292feee3a8f083d03a9aec3d46ef5
OtavioFilipe <darksnack16@gmail.com>
2022-09-27 19:19:14 -0300

    add skeleton

--- App.tsx
+++ App.tsx
@@ -2,7 +2,22 @@ import React from 'react';
 import HomeScreen from './src/screens/home';
 
 const App = () => {
-  return <HomeScreen />;
+  return (
+    <>
+      <HomeScreen />
+      {/* <SkeletonPlaceholder>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{width: 60, height: 60, borderRadius: 50}} />
+          <View style={{marginLeft: 20}}>
+            <View style={{width: 120, height: 20, borderRadius: 4}} />
+            <View
+              style={{marginTop: 6, width: 80, height: 20, borderRadius: 4}}
+            />
+          </View>
+        </View>
+      </SkeletonPlaceholder> */}
+    </>
+  );
 };
 
 export default App;
